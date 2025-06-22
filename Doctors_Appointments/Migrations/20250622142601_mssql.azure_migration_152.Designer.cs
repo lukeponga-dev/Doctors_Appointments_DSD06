c65bb6d82c7be592ec0f8d979e9f2bde70c7fe35
lukeponga-dev <developmentdesignsltd@gmail.com>
2025-06-23 03:32:39 +1200

    Update CI/CD workflows and .NET project configurations
    - Updated Azure Static Web Apps workflows for `icy-wave` and `orange-stone` with specific API tokens and retained pull request handling.
    - Modified .NET workflow to target .NET 8.0, enhancing build, test, and publish steps.
    - Improved Jekyll GitHub Pages deployment workflow with new build and configuration steps.
    - Updated `Doctors_Appointments.csproj` and related files to use .NET 8.0 and newer Entity Framework Core packages.
    - Cleaned up Azure Resource Manager templates for `mssql1` and `mssql2`, aligning with new deployment structure.
    - Revised `serviceDependencies` files to reflect updated dependencies and added app service connections.
    - Created a new YAML workflow for building and deploying the .NET Core application to Azure Web App.
    - Added new migration files for Entity Framework Core to reflect schema changes.
    - Defined ARM template for compute app service to set up necessary resources for deployment.

--- Doctors_Appointments/Migrations/20250622142601_mssql.azure_migration_152.Designer.cs
+++ Doctors_Appointments/Migrations/20250622142601_mssql.azure_migration_152.Designer.cs
@@ -4,14 +4,16 @@ using Doctors_Appointments.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Doctors_Appointments.Migrations
 {
     [DbContext(typeof(Doctors_AppointmentsContext))]
-    partial class Doctors_AppointmentsContextModelSnapshot : ModelSnapshot
+    [Migration("20250622142601_mssql.azure_migration_152")]
+    partial class mssqlazure_migration_152
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Doctors_Appointments/Migrations/20250622142601_mssql.azure_migration_152.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Doctors_Appointments.Migrations
+{
+    public partial class mssqlazure_migration_152 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
